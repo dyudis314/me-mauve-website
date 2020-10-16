390d6a01a20295c05da9b44de9df250ac8260399
Dylan Cole <dyudis314@gmail.com>
2020-10-16 16:45:37 -0700

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+/* Scroll on buttons */
+$('.js--scroll-to-song').click(function () {
+$('html, body').animate({scrollTop: $('.js--section-promo').offset().top}, 1000);
+});
+
+$('.js--scroll-to-contact').click(function () {
+  $('html, body').animate({scrollTop: $('.js--section-mailing-list').offset().top}, 1000);
+  });
+
+/* Mobile Navigation */
+
+
+   
+});
+
+
+
