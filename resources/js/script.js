1e6a87756f1fb982850a3891f40d85cc5bac8b3c
Dylan Cole <dyudis314@gmail.com>
2020-11-24 11:12:25 -0800

    Style Updates
    -Fixed padding / margin
    -Made nav bar sticky and created 'disappear on scroll' function

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,6 +17,62 @@ burger.addEventListener('click', () => {
 navSlide();
 
 
+/* When user scrolls down, hide navbar. When user scrolls up, show navbar. */
+(function(){
+
+  let doc = document.documentElement;
+  let w   = window;
+
+  let curScroll;
+  let prevScroll = w.scrollY || doc.scrollTop;
+  let curDirection = 0;
+  let prevDirection = 0;
+
+
+  let header = document.querySelector('nav');
+  let toggled;
+  let threshold = 200;
+
+  const checkScroll = function() {
+      curScroll = w.scrollY || doc.scrollTop;
+      if(curScroll > prevScroll) {
+          // scrolled down
+          curDirection = 2;
+      }
+      else {
+          //scrolled up
+          curDirection = 1;
+      }
+
+      if(curDirection !== prevDirection) {
+          toggled = toggleHeader();
+      }
+
+      prevScroll = curScroll;
+      if(toggled) {
+          prevDirection = curDirection;
+      }
+  };
+
+  const toggleHeader = function() { 
+      toggled = true;
+      if(curDirection === 2 && curScroll > threshold) {
+          header.classList.add('hide');
+      }
+      else if (curDirection === 1) {
+          header.classList.remove('hide');
+      }
+      else {
+          toggled = false;
+      }
+      return toggled;
+  };
+
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
+
 /* Reload on hero text box click */
  function reloadOnClick() {
   const hero = document.querySelector('.hero-text-box');
