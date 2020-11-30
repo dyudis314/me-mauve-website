c9b882ae158846740a137a1ba8458f10a038b2c2
Dylan Cole <dyudis314@gmail.com>
2020-11-30 10:00:33 -0800

    Code Cleanup Before Launch

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,4 @@
 /* Nav Sidebar Slide */
-
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
@@ -14,56 +13,6 @@ burger.addEventListener('click', () => {
     });
   }
 navSlide();
-
-/* When user scrolls down, hide navbar. When user scrolls up, show navbar. */
-  (function(){
-    let doc = document.documentElement;
-    let w   = window;
-
-    let curScroll;
-    let prevScroll = w.scrollY || doc.scrollTop;
-    let curDirection = 0;
-    let prevDirection = 0;
-
-
-    let header = document.querySelector('nav');
-    let toggled;
-    let threshold = 200;
-
-    const checkScroll = function() {
-      curScroll = w.scrollY || doc.scrollTop;
-      if(curScroll > prevScroll) {
-          // scrolled down
-          curDirection = 2;
-      }
-      else {
-          //scrolled up
-          curDirection = 1;
-      }
-      if(curDirection !== prevDirection) {
-          toggled = toggleHeader();
-      }
-      prevScroll = curScroll;
-      if(toggled) {
-          prevDirection = curDirection;
-      }
-  };
-
-  const toggleHeader = function() { 
-      toggled = true;
-      if(curDirection === 2 && curScroll > threshold) {
-          header.classList.add('hide');
-      }
-      else if (curDirection === 1) {
-          header.classList.remove('hide');
-      }
-      else {
-          toggled = false;
-      }
-      return toggled;
-  };
-  window.addEventListener('scroll', checkScroll);
-})();
 
 /* Reload on hero text box click */
  function reloadOnClick() {
