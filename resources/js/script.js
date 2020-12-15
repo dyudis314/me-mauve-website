95ca271bdd5be78898c43f8644eb620edb7c150d
Dylan Cole <dyudis314@gmail.com>
2020-12-15 11:40:14 -0800

    quick changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,27 @@
-$(document).ready(function() {
+/* Nav Sidebar slide */
 
-/* Scroll on buttons */
-$('.js--scroll-to-song').click(function () {
-$('html, body').animate({scrollTop: $('.js--section-promo').offset().top}, 1000);
-});
-
-$('.js--scroll-to-contact').click(function () {
-  $('html, body').animate({scrollTop: $('.js--section-mailing-list').offset().top}, 1000);
-  });
-
-});
-
-
-
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav links li');
+  
+    // When burger is clicked, nav-active class is toggled on/off.
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+  
+      // Burger Animation
+      burger.classList.toggle('toggle');
+  
+      });
+    }
+  navSlide();
+  
+  
+  /* Reload on hero text box click */
+   function reloadOnClick() {
+    const hero = document.querySelector('.hero-text-box');
+    hero.addEventListener('click', () => {
+      location.reload();
+    })
+   }
+   reloadOnClick();
